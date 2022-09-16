dfdeef36ea91c99c4d4984e1aa7c2021173c8fd4
Javier Sanchez <javier.san.mail@gmail.com>
2022-09-16 15:52:25 +0200

    :art: Set Flex to editor

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST={"challenges/[id].tsx -> ../../components/CodeEnviroment/CodeEnviroment":{"id":"challenges/[id].tsx -> ../../components/CodeEnviroment/CodeEnviroment","files":["static/chunks/components_CodeEnviroment_CodeEnviroment_tsx.js"]}}
+self.__REACT_LOADABLE_MANIFEST={"challenges/[id].tsx -> ../../components/CodeEnviroment/CodeEnviroment":{id:6014,files:["static/chunks/4ad82c5e.4c8609673428f957.js","static/chunks/151-6f7c238f53209f0c.js","static/chunks/155.35418fc29011967f.js","static/chunks/14.dc80fb67cec2b0b2.js"]}}
