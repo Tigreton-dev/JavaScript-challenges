c094dbca748eccafedd47e40dd47b0c3e6a113da
Javier Sanchez <javier.san.mail@gmail.com>
2022-12-24 15:46:07 +0100

    :heavy_plus_sign: add Ubuntu font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,11 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
+
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+                    />
                 </Head>
                 <body>
                     <Main />
