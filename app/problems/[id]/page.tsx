3e07f1d93046b8a7ce300f1301f650f19ae0eebd
javi Sanchez <javier.san.mail@gmail.com>
2024-02-16 21:25:02 +0100

    ad new relative route

---
+++ app/problems/[id]/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React, { useEffect, useRef, useState } from "react";
+import NavBar from "../../components/navBar/navBar"
+import Split from 'react-split'
+import ProblemDescription from "../../components/problemDescription";
+import DevelopmentEnvironment from "../../components/codeEditor/developmentEnvironment";
+import { DataContext } from '../../context/dataContext';
+import Firework from "../../components/firework";
+import { usePathname } from 'next/navigation'
+import { getProblemByRefName } from "../../data/getData"
+
+export default function ProblemPage() {
+    const { data, updateData } = React.useContext(DataContext);
+    const sliptRef = useRef(null)
+    const mainRef = useRef(null)
+    const pathname = usePathname()
+    const [isProblemSet, setProblem] = useState(false)
+
+    useEffect(() => {
+        setProblem(() => false)
+        const pathnameSplited = pathname.split("/");
+        const problemNameId = pathnameSplited[pathnameSplited.length - 1];
+        const problem = getProblemByRefName(problemNameId)
+        updateData({ currentProblem: problem })
+        setTimeout(() => {
+            setProblem(() => true)
+        }, 500);
+    }, []);
+
+    useEffect(() => {
+        if (sliptRef.current === null) return
+        data.isFullScreen ? sliptRef.current.parent.classList.add("splitFullScreen") : sliptRef.current.parent.classList.remove("splitFullScreen")
+    }, [data.isFullScreen])
+
+    useEffect(() => {
+        const htmlElement = document.getElementById("htmlElement") as HTMLElement
+        htmlElement.style.fontSize = `${data.appSize}px`
+    }, [data.appSize])
+
+    return (
+
+        <main className="h-[100vh] flex flex-col overflow-hidden" ref={mainRef}>
+            <NavBar />
+            {isProblemSet ? <>
+                <Split
+                    ref={sliptRef}
+                    className="split grow overflow-hidden"
+                    sizes={[45, 55]}
+                    minSize={100}
+                    expandToMin={false}
+                    gutterSize={10}
+                    gutterAlign="center"
+                    snapOffset={30}
+                    dragInterval={1}
+                    direction="horizontal"
+                    cursor="col-resize"
+                >
+                    <ProblemDescription />
+                    <DevelopmentEnvironment />
+                </Split>
+                <Firework />
+            </> : <p>Loading problem...</p>}
+
+        </main>
+    );
+}
