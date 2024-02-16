e47e6234b20f612df1f7a84b147ba59b5f3e4b12
javi Sanchez <javier.san.mail@gmail.com>
2024-02-16 21:19:03 +0100

    change logic on current home page removing logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,11 @@
-"use client"
-import React, { useEffect, useRef } from "react";
-import NavBar from "../app/NavBar"
-import Split from 'react-split'
-import ProblemDescription from "./problemDescription";
-import RightSide from "./rightSide";
-import { DataContext } from './context/dataContext';
-import Firework from "./firework";
+'use client'
+import ProblemList from "./components/navBar/problemListModal";
 
 export default function Home() {
-	const { data, updateData } = React.useContext(DataContext);
-	const sliptRef = useRef(null)
-	const mainRef = useRef(null)
-
-	useEffect(() => {
-		data.isFullScreen ? sliptRef.current.parent.classList.add("splitFullScreen") : sliptRef.current.parent.classList.remove("splitFullScreen")
-	}, [data.isFullScreen])
-
-	useEffect(() => {
-		const htmlElement = document.getElementById("htmlElement")
-		htmlElement.style.fontSize = `${data.appSize}px`
-		// htmlElement.style.setProperty("--nav-bar-height", String(allSdkRatio));
-	}, [data.appSize])
 
 	return (
-		<main className="h-[100vh] flex flex-col overflow-hidden" ref={mainRef}>
-			<NavBar />
-			<Split
-				ref={sliptRef}
-				className="split grow overflow-hidden"
-				sizes={[45, 55]}
-				minSize={100}
-				expandToMin={false}
-				gutterSize={10}
-				gutterAlign="center"
-				snapOffset={30}
-				dragInterval={1}
-				direction="horizontal"
-				cursor="col-resize"
-			>
-				<ProblemDescription />
-				<RightSide />
-			</Split>
-			<Firework />
+		<main className="h-[100vh] flex flex-col overflow-hidden">
+			<ProblemList openOnRender={true} />
 		</main>
 	);
 }
