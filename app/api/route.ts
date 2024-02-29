04901d58497df0b3d7656f2cfef88898a05b7bd9
javi Sanchez <javier.san.mail@gmail.com>
2024-02-29 16:11:56 +0100

    update endpoint to return problem with submitted code if exist

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,6 @@
 import { headers } from 'next/headers';
 import { list } from '@vercel/blob';
+import { sql } from '@vercel/postgres';
 
 export async function GET(request: Request) {
     const allBlobs = await list();
@@ -7,10 +8,18 @@ export async function GET(request: Request) {
     const problems = await getProblesResponse.json();
     const headersList = headers();
     const problemNameId = headersList.get('problemNameId') as string;
+    const user_id = headersList.get('user_id') as string;
     for (const challenge in problems) {
         const currChallenge = problems[challenge];
         if (currChallenge.refName === problemNameId) {
-            return Response.json({ ...currChallenge });
+            if (user_id === 'null') return Response.json({ ...currChallenge });
+            const isChallengeSubmitted =
+                await sql`SELECT submitted_code FROM submitted_code WHERE user_id = ${user_id} AND challenge_id = ${currChallenge.refNumber}`;
+            if (isChallengeSubmitted.rows.length && isChallengeSubmitted.rows[0].submitted_code) {
+                return Response.json({ ...currChallenge, submittedCode: isChallengeSubmitted.rows[0].submitted_code });
+            } else {
+                return Response.json({ ...currChallenge });
+            }
         }
     }
 }
