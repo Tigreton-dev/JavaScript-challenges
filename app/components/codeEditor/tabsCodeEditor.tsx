51ee8a32271cfe980fa5a79f43b71510851d2f27
javi Sanchez <javier.san.mail@gmail.com>
2024-03-01 18:50:21 +0100

    add subbmitted code tab at code tabs component

--- app/components/codeEditor/tabsCodeEditor.tsx
+++ app/components/codeEditor/tabsCodeEditor.tsx
@@ -2,8 +2,12 @@ import React, {useState} from "react";
 import { Tabs, Tab, Navbar, Button, DropdownTrigger } from "@nextui-org/react";
 import { SettingsIcon, DescriptionIcon, CodeIcon, CopiedTick, CopyIcon, ReloadIcon, FullScreenIcon } from "../../helpers/Icons";
 import SettingsCodeEditor from "./SettingsCodeEditor"
+import { DataContext } from '../../context/dataContext';
 
 export default function TabCodeEditorComponent({ isClipBoardClicked, onTabChange, prettifyCode, resetCode, copyCode, setFullScreen }) {
+    const { data } = React.useContext(DataContext);
+    const isSolutionSubmitted = typeof data.currentProblem.submittedCode === "string";
+
     return (
         <Navbar isBlurred={false} maxWidth="full" className="h-12">
             <div className="flex w-full flex-col">
@@ -28,6 +32,16 @@ export default function TabCodeEditorComponent({ isClipBoardClicked, onTabChange
                             </div>
                         }
                     />
+                    {isSolutionSubmitted &&  <Tab
+                        key="1"
+                        title={
+                            <div className="flex items-center space-x-2">
+                                <DescriptionIcon />
+                                <span>Submitted Solution</span>
+                            </div>
+                        }
+                    />}
+                   
 
                 </Tabs>
 
