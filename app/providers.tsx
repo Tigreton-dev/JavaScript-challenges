c20f85a14584b0ee0b13ba52d9e30ac4975c5cd3
javi Sanchez <javier.san.mail@gmail.com>
2024-03-21 08:40:31 +0100

    small refactor

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,13 @@
 // app/providers.tsx
-"use client";
+'use client';
 
-import { NextUIProvider } from '@nextui-org/react'
-import { SessionProvider } from "next-auth/react";
+import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <NextUIProvider>
-            <SessionProvider>
-                {children}
-            </SessionProvider>
+            <SessionProvider>{children}</SessionProvider>
         </NextUIProvider>
-
-    )
+    );
 }
