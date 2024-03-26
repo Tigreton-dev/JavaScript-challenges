f88805571f8bacf7e8658283fdf6e1ec7dec2f75
javi Sanchez <javier.san.mail@gmail.com>
2024-03-26 10:30:30 +0100

    add challenge description component

--- app/components/challengeDescription/index.tsx
+++ app/components/challengeDescription/index.tsx
@@ -7,18 +7,18 @@ import SolutionCode from './solutionCode';
 import Description from './description';
 
 export default function ChallengeDescription({ currentChallenge }) {
-    const [index, setIndex] = useState<number>(0);
+   const [index, setIndex] = useState<number>(0);
 
-    return (
-        <div className="border border-default-300 dark:border-default-100 overflow-hidden rounded-lg">
-            <MackOsTitleBar />
-            <div className="overflow-scroll h-[calc(100%-2rem)] relative">
-                <TabComponent onTabChange={(i: number) => setIndex(i)} />
-                {index === 0 && <Description currentChallenge={currentChallenge} />}
-                {index === 1 && <SolutionCode currentChallenge={currentChallenge} />}
-                {index === 2 && <TestCases challengeId={currentChallenge.refName} />}
-                {/* {index === 3 && <VideoSolutions />} */}
-            </div>
-        </div>
-    );
+   return (
+      <div className="border border-default-300 dark:border-default-100 overflow-hidden rounded-lg">
+         <MackOsTitleBar />
+         <div className="overflow-scroll h-[calc(100%-2rem)] relative">
+            <TabComponent onTabChange={(i: number) => setIndex(i)} />
+            {index === 0 && <Description currentChallenge={currentChallenge} />}
+            {index === 1 && <SolutionCode currentChallenge={currentChallenge} />}
+            {index === 2 && <TestCases challengeId={currentChallenge.refName} />}
+            {/* {index === 3 && <VideoSolutions />} */}
+         </div>
+      </div>
+   );
 }
